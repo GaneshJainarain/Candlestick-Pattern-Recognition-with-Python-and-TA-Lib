f7684162c1c8bfa79dd347741781b38243e8ad85
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-10-29 00:00:59 -0400

    created the index.html route

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 
 @app.route('/snapshot')
