9a5dc58968a1983b2dfbabcc127c30d5a2afd581
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-10-28 23:57:34 -0400

    added a Templates folder where we will start writing some html code for the frontend

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/snapshot')
+def snapshot():
+    return {
+        'code': 'success'
+    }
