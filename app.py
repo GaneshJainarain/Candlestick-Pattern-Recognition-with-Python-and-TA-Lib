780c83d62b9386937ea32693b5401f6506fbf957
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-10-29 00:34:12 -0400

    making patterns dynamic got all the patterns from TA Lib website, used regex structure to format properly, having trouble importing the modules from patterns

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from patterns import patterns
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', patterns=patterns)
 
 
 @app.route('/snapshot')
