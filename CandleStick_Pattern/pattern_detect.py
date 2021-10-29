42a22ad706980cbb12f21197fe70d7fa99c73848
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-10-28 23:42:57 -0400

    modified:   CandleStick_Pattern/pattern_detect.py installed flask

--- CandleStick_Pattern/pattern_detect.py
+++ CandleStick_Pattern/pattern_detect.py
@@ -5,6 +5,14 @@ data = yf.download("SPY", start="2021-01-01", end="2021-10-22")
 
 #print(data)
 
-num = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
 
-print(num[num !=0])
+engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+
+data['Morning_Star'] = morning_star
+data['Engulfing'] = engulfing
+
+print(data)
+print(morning_star[morning_star !=0])
+engulfing_days = data[data['Engulfing'] !=0]
+print(engulfing_days)
