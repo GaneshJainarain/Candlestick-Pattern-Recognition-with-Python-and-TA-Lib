42685946d100d78ed9da6be480ec1e7fd737042f
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-11-03 19:20:53 -0400

    added in the app.py the data frame that gets the stokcs from a starting to end time and saves them in the datasets/daily folder

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ def hello_world():
 @app.route('/snapshot')
 def snapshot():
     with open('datasets/companies.csv') as f:
-        symbols = f.read().splitlines()
-        print(symbols)
+        companies = f.read().splitlines()
+        for company in companies:
+            symbol = company.split(',')[0]
+            df = yf.download(symbol, start="2020-01-01", end="2020-08-01")
+            df.to_csv('datasets/daily/{}.csv'.format(symbol))
     return {
         'code': 'success'
     }
